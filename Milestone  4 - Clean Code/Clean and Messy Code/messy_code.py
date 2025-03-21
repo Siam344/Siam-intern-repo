4b8dfde9e4fd6c736a3896edf5339617e4128a07
Siam344 <siamenur@gmail.com>
2025-03-22 01:10:04 +1100

    Add clean code principles and messy code examples

---
+++ Milestone  4 - Clean Code/Clean and Messy Code/messy_code.py
@@ -0,0 +1,11 @@
+def calc(a,b,c):
+    if a > 5:
+        if b < 10:
+            print(a + b * c)
+        else:
+            print(a * b / c)
+    else:
+        if c > 0:
+            print(a - b + c)
+        else:
+            print("Error")
